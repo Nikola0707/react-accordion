ebcd061f71dc993f8aeeef32156194b1de122943
Nikola0707 <ristoski.nikola@gmail.com>
2021-01-06 13:00:23 +0100

    Finished Accordion project

--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,22 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+const Question = ({ title, info }) => {
+  const [showInfo, setshowInfo] = useState(false)
+
+  const changeInfoState = () => setshowInfo(!showInfo)
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={changeInfoState}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  )
 };
 
 export default Question;
